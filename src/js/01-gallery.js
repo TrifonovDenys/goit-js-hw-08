caa2df28ccec42b4f4711990ee82659e5f013d10
Trifonov Denys <trifonowden123@gmail.com>
2023-05-06 19:42:53 +0300

    exercise 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+const createGalleryTable = () => {
+  const galleryTable = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src='${preview}' data-source='${original}' alt='${description}'></a></li>`
+    )
+    .join('');
+
+  return galleryTable;
+};
+
+galleryBox.insertAdjacentHTML('beforeend', createGalleryTable());
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+  scrollZoom: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
