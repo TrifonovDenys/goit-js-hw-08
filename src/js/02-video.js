d7965cd2bf31d5eb8424ff1f5027a51094d7c88f
Trifonov Denys <trifonowden123@gmail.com>
2023-05-07 02:08:38 +0300

    exercise 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time'
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function recordCurrentTime({ seconds, ...other }) {
+  localStorage.setItem(CURRENT_TIME_KEY, seconds);
+}
+
+player.on('timeupdate', throttle((recordCurrentTime), 1000))
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY))
